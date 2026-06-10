3c14d0220f66183db76abc37a1ab1a2ebdb34482
lyf <liyifeng0039@gmail.com>
2026-06-10 20:15:12 +0800

    fixing

--- modules/aspects/system/core/wsl.nix
+++ modules/aspects/system/core/wsl.nix
@@ -5,24 +5,32 @@
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  # TODO： 1.wsl需要拆分user; 2. 移动到更合适的位置
   den.aspects.system.core =
     { host, user }:
     {
-      nixos = lib.mkIf (host.env == "wsl" && host.distro == "nixos") {
-        imports = [ inputs.nixos-wsl.nixosModules.wsl ];
-        wsl = {
-          enable = true;
-          useWindowsDriver = true;
-          startMenuLaunchers = true;
-          defaultUser = user.userName;
+      nixos =
+        { ... }:
+        let
+          wslHost = host.env == "wsl" && host.distro == "nixos";
+        in
+        {
+          imports = lib.optionals wslHost [ inputs.nixos-wsl.nixosModules.wsl ];
+        }
+        // lib.optionalAttrs wslHost {
+          wsl = {
+            enable = true;
+            useWindowsDriver = true;
+            startMenuLaunchers = true;
+            defaultUser = user.userName;
 
-          wslConf.automount.root = "/mnt";
-          wslConf.network.hostname = host.hostName;
+            wslConf.automount.root = "/mnt";
+            wslConf.network.hostname = host.hostName;
+          };
+
+          # boot.loader.systemd-boot.enable = false;
+          # boot.loader.efi.canTouchEfiVariables = false;
         };
-
-        # boot.loader.systemd-boot.enable = false;
-        # boot.loader.efi.canTouchEfiVariables = false;
-      };
     };
 }
 
